a8f30446adec16fcfcb66cc36aa080bf45966016
Christoph Voigt <voigt.christoph@gmail.com>
2023-10-13 15:39:48 +0200

    add error handling

--- env-explorer/src/handlers/get_conf_vals.rs
+++ env-explorer/src/handlers/get_conf_vals.rs
@@ -10,11 +10,16 @@ use crate::models::ConValListModel;
 pub(crate) fn handle_get_conf_vals(_req: Request, _params: Params) -> Result<Response> {
     let mut confvals: Vec<ConValListModel> = Vec::<ConValListModel>::default();
 
+    let v_result = config::get("environment");
+    match v_result {
+        Ok(v) =>     confvals.push(ConValListModel{
+            key: "environment".to_string(),
+            value: v,
+        }),
+        Err(error) => panic!("Problem getting environment: {:?}", error)   
+    };
+
     // TODO: implement config::get_all() in spin_sdk instead
-    confvals.push(ConValListModel{
-        key: "environment".to_string(),
-        value: config::get("environment").expect("Failed to acquire environment"),
-    });
 
     let body = serde_json::to_string(&confvals)?;
     Ok(http::Response::builder()
